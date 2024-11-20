05e0a42e18e2eb6a3b708780a0cec8010241a0e6
SaraGB <lgg73434@educastur.es>
2024-11-20 10:36:24 +0100

    Repositorio creado

---
+++ tarea3dwesSara/src/main/java/com/saragb/tarea3dwesSara/Tarea3dwesSaraApplication.java
@@ -0,0 +1,13 @@
+package com.saragb.tarea3dwesSara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea3dwesSaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea3dwesSaraApplication.class, args);
+	}
+
+}
---
+++ tarea3dwesSara/src/test/java/com/saragb/tarea3dwesSara/Tarea3dwesSaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saragb.tarea3dwesSara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea3dwesSaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
